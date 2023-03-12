bc137608e4f65381c0e06518df400a41bca04993
hyojae15 <tls15gywo@gmail.com>
2023-03-12 16:06:50 +0900

    [update] 데이터 프로세싱 ver0.1

--- dataProcessing.py
+++ dataProcessing.py
@@ -1,13 +1,11 @@
 """
-
 저장된 좌표 자료들(좌표, OR)과 비교값 자료들(소요시간, IoU)의 활용 방안
     
     1. 좌표 파일: Coordinate Data
+        저장된 좌표 갯수가 탐지 횟수임, 각 이미지에 대한 MOR(Mean Overlap Rate) 계산
 
     2. 비교값 파일: Comparative Data
-
-한개의 파일로 만들어서 결과들을 한눈에 보고싶다 그지??
-
+        
 """
 
 import os
@@ -26,8 +24,9 @@ from py_script.utils.utils import *
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("coordinateData_path", help="folder path to Coordinate for Coordinate Data", type=str)
-parser.add_argument("comparativeData_path", help="file path to comparativeData for Comparative Data", type=str)
+parser.add_argument("labeler_name", help="Typing your name", type=str)
+parser.add_argument("coordinateData_path", help="Folder path to Coordinate for Coordinate Data", type=str)
+parser.add_argument("comparativeData_path", help="File path to comparativeData for Comparative Data", type=str)
 
 # parser.add_argument("--target_class_num", default=1, type=int)
 
@@ -37,6 +36,7 @@ args = parser.parse_args()
 def main():
     print("dataProcessing")
     
+    labeler_name = args.labeler_name
     coordinateData_path = args.coordinateData_path
     comparativeData_path = args.comparativeData_path
 
@@ -66,13 +66,18 @@ def main():
             print(f"float_OR_list: {float_OR_list}")
             MOR = sum(float_OR_list)/len(float_OR_list)
             print(f"MOR: {MOR}")
-            MOR_list.append([file_name, MOR])
+            MOR_list.append([file_name, len(float_OR_list), MOR])
     print(f"MOR: {MOR_list}, len: {len(MOR_list)}")
 
     
     # comparativeAnalysis = open(os.path.join(comparativeData_path, "analysisData.csv"), "w", encoding="cp949", newline="")
-    fields = ["File", "MOR"]
-    with open(os.path.join(comparativeData_path, "analysisData.csv"), "w", encoding="cp949", newline="") as f:
+    fields = ["File", "Num of detect", "MOR"]
+    
+    analysisData_filename = f"{labeler_name}_analysisData.csv"
+    # analysisData_filename.format(name=labeler_name)
+    analysisData = os.path.join(comparativeData_path, analysisData_filename)
+    
+    with open(analysisData, "w", encoding="cp949", newline="") as f:
         write = csv.writer(f)
         write.writerow(fields)
         write.writerows(MOR_list)
